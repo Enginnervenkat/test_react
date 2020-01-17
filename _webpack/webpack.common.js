784526e856726da309a1e7f1d5ee1c61792a3e37
venkat <venkatlapc@gmail.com>
2020-01-17 23:13:26 +0530

    feat: added template,dev server

--- _webpack/webpack.common.js
+++ _webpack/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -18,5 +19,12 @@ module.exports = {
         },
       },
     ],
+
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My website',
+      template: path.resolve(__dirname, '../_templates/client.html'),
+    }),
+  ],
 };
